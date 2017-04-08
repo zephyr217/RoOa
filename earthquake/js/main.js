41572d68eeb7d9b82d9fafeb4bea61bcf58e77f0
Oana <rolling_chords@yahoo.com>
2017-04-09 00:44:18 +0300

    get eqk date

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,11 +1,14 @@
 
-$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson", function( data ) {
+$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson", function( data ) {
   console.log(data);
   $(document).ready(function() {
         $(data.features).each(function (index, value){
             console.log(value.properties.place)
             var str = value.properties.place
             var LocationName=str.substring(str.lastIndexOf(",")+2,str.lastIndexOf(""))
+            var d = new Date(value.properties.time)
+            var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+            d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
             $("#search-button").click(function() {
                 var country = document.getElementById('autocomplete-ajax').value;
                 if(country==LocationName)
