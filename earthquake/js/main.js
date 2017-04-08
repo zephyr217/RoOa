4f865b7d19e3fe8ee15b968407b73e4c45c908bb
MuscaliucRobert <muscaliucrobert@gmail.com>
2017-04-08 23:03:46 +0300

    try to take the value from form

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,13 +1,19 @@
-$( "#1" ).css( "border", "3px solid red" );
-$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function( data ) {
-  $( "#result" ).html( JSON.stringify(data) );
 
-  console.log( data);
+
+$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function( data ) {
+
+
+  console.log(data);
+  $(document).ready(function() {
+        $('#search-button').click(function() {
+           console.log ( $('#autocomplete-ajax').val())
+        });
+    });
  
      $(data.features).each(function (index, value){
          console.log(value.properties.place)
           var str = value.properties.place
-          var LocationName=str.substring(str.lastIndexOf(",")+2,str.lastIndexOf(""));
+          var LocationName=str.substring(str.lastIndexOf(",")+2,str.lastIndexOf(""))
           console.log(LocationName)
      });
     
