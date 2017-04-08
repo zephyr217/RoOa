5e3ba66fcf74a6d0e69b36b5a433bb6fd8981b72
MuscaliucRobert <muscaliucrobert@gmail.com>
2017-04-09 00:04:44 +0300

    table works

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,23 +1,26 @@
 
 
-$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function( data ) {
+$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson", function( data ) {
 
 
   console.log(data);
   $(document).ready(function() {
-        $('#search-button').click(function() {
-           console.log ( $('#autocomplete-ajax').val())
-        });
-    });
- 
-     $(data.features).each(function (index, value){
-         console.log(value.properties.place)
-          var str = value.properties.place
-          var LocationName=str.substring(str.lastIndexOf(",")+2,str.lastIndexOf(""))
-          console.log(LocationName)
+      
+
+        $(data.features).each(function (index, value){
+            console.log(value.properties.place)
+            var str = value.properties.place
+            var LocationName=str.substring(str.lastIndexOf(",")+2,str.lastIndexOf(""))
+            $("#search-button").click(function() {
+                var country = document.getElementById('autocomplete-ajax').value;
+                if(country==LocationName)
+                {
+                    $('#country-table > tbody:last-child').append('<tr><td>'+country+'</td></tr>').html();
+                }
+               
+          });    
      });
-    
-  
+    });
 }); 
 
 
