2e5e1a6e885f211236826931bdf4a8aed99005f9
MuscaliucRobert <muscaliucrobert@gmail.com>
2017-04-08 19:40:30 +0300

    Citire locatie din JSON

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,5 +1,14 @@
 $( "#1" ).css( "border", "3px solid red" );
-$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function( data ) {
+$.getJSON( "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=5", function( data ) {
   $( "#result" ).html( JSON.stringify(data) );
-  console.log( data);
-});
+ console.log(data)
+ 
+  console.log(data.features["0"].properties.place)
+ 
+ 
+     $(data.features).each(function (index, value){
+         console.log(value.properties.place)
+     });
+  
+}); 
+
