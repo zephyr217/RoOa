9bfe93d6a1e502e67e130ba74e88c579d834fbea
MuscaliucRobert <muscaliucrobert@gmail.com>
2017-04-09 00:45:58 +0300

    magnitudine

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,16 +1,20 @@
 
-$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson", function( data ) {
+$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson", function( data ) {
   console.log(data);
   $(document).ready(function() {
         $(data.features).each(function (index, value){
             console.log(value.properties.place)
             var str = value.properties.place
+            var magn = value.properties.mag
+            var UnixDate = new Date (value.properties.time)
+            console.log(UnixDate)
             var LocationName=str.substring(str.lastIndexOf(",")+2,str.lastIndexOf(""))
+            console.log(magn)
             $("#search-button").click(function() {
                 var country = document.getElementById('autocomplete-ajax').value;
                 if(country==LocationName)
                 {
-                    $('#country-table > tbody:last-child').append('<tr><td>'+country+'</td></tr>').html();
+                    $('#country-table > tbody:last-child').append('<tr><td>'+country+'</td><td>'+magn+'</td></tr>').html();
                 }               
           });    
        });
