eb8e1959b5475619f68f4eca8364cdc646cf4276
Oana <rolling_chords@yahoo.com>
2017-04-08 16:10:19 +0300

    bootstrap

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,1 +1,5 @@
 $( "#1" ).css( "border", "3px solid red" );
+$.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function( data ) {
+  $( "#result" ).html( JSON.stringify(data) );
+  console.log( data);
+});
