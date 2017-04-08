eed59d3030558b0ca81feb0c31e7d0fb11f2e4cc
Oana <rolling_chords@yahoo.com>
2017-04-09 00:08:38 +0300

    removed some unnecessary stuff

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,12 +1,7 @@
 
-
 $.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson", function( data ) {
-
-
   console.log(data);
   $(document).ready(function() {
-      
-
         $(data.features).each(function (index, value){
             console.log(value.properties.place)
             var str = value.properties.place
@@ -16,10 +11,9 @@ $.getJSON( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significan
                 if(country==LocationName)
                 {
                     $('#country-table > tbody:last-child').append('<tr><td>'+country+'</td></tr>').html();
-                }
-               
+                }               
           });    
-     });
+       });
     });
 }); 
 
